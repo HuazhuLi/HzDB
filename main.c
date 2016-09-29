a17fd90e5fcf702a3a545a07ad0434c9a095518d
HuazhuLi,李华柱 <huazhuhhfuufli@gmail.com>
2016-09-29 20:12:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -48,3 +48,35 @@ typedef struct DB
     COUNT cnt_stor4;
     COUNT cnt_storerr;
 };
+
+/*internal functions*/
+static DB *_db_alloc(int);
+static void _db_dodelete(DB *);
+static int _db_find_and_lock(DB *,const char *, int);
+static int _db_findfree(DB*, int, int);
+static void _db_free(DB *);
+static DBHASH _db_hash(DB *, const char*);
+static char *_db_readdat(DB *);
+static off_t	*_db_readidx(DB *);
+static off_t *_db_readptr(DB *, off_t);
+static void _db_writedat(DB *, const char *, off_t, int);
+static void _db_writeidx(DB *, const char *, off_t, int, off_t);
+static void _db_writeptr(DB *, off_t, off_t);
+
+DBHANDLE
+db_open(const char *pathname, int oflag, ...)/*什么时候改用指针什么时候不用？如何沿用另外一种更加直观的命名方法？*/
+{
+	DB  *db;
+	int len, mode;
+	size_t i;
+	char  asciiptr[PTR_SZ + 1];
+		  hash[(NHASH_DEF + 1) * PTR_SZ + 2];//偏移地址除了改变映射指向以外还有其他作用吗？
+
+	struct stat statbuff;//结构为什么有时候有有时候没有？c中的关键字不熟悉导致了更多的问题
+
+	len = strlen(pathname);
+	if ((db = _db_alloc(len)) == NULL)//表达式等于null，什么意思？
+	{
+		err_dump("db_open: _db_alloc error for DB");
+	}
+}
